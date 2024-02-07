a8da0ed429f9b041e69fa8bd45ee02dc6900ba9c
Vishal V <svis@softwareag.com>
2024-02-07 16:04:09 +0530

    change in backend

--- cafeteria-spring/src/main/java/com/softwareag/cafeteria/CafeteriaApplication.java
+++ cafeteria-spring/src/main/java/com/softwareag/cafeteria/CafeteriaApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CafeteriaApplication {
 
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(CafeteriaApplication.class, args);
 	}
