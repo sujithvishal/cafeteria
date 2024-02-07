3003ee8e14d7334811294d04ecd2681fdd6eab62
Vishal V <svis@softwareag.com>
2024-02-07 12:56:14 +0530

    change in test

--- cafeteria-spring/src/test/java/com/softwareag/cafeteria/CafeteriaApplicationTests.java
+++ cafeteria-spring/src/test/java/com/softwareag/cafeteria/CafeteriaApplicationTests.java
@@ -1,13 +1,44 @@
 package com.softwareag.cafeteria;
 
+import com.softwareag.cafeteria.model.Employee;
+import com.softwareag.cafeteria.model.Product;
+import com.softwareag.cafeteria.repository.EmployeeRepository;
+import com.softwareag.cafeteria.repository.ProductRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class CafeteriaApplicationTests {
 
+	@Autowired
+	EmployeeRepository employeeRepository;
+
+	@Autowired
+	ProductRepository productRepository;
+
 	@Test
-	void contextLoads() {
+	public void testCreateEmployee(){
+
+		Employee e =new Employee("5005","kumar","1234",200,"USER");
+		employeeRepository.save(e);
+		assertNotNull(employeeRepository.findById("5005").get());
+
+	}
+
+	@Test
+	public void testCreateProduct(){
+
+		Product p =new Product(null,"tea",15,"sampleurl");
+		productRepository.save(p);
+		List<Product> list =productRepository.findAll();
+		assertThat(list).size().isGreaterThan(0);
+
 	}
 
 }
